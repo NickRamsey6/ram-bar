93e5be4b7e98bdceecc0935258fde1bd180662b3
Nick Ramsey <nramseysc@gmail.com>
2020-07-09 00:20:51 -0700

    nav bar v2

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -28,19 +28,19 @@ class TopNav extends React.Component {
                 <NavbarBrand style={{fontWeight: "bold"}} href='/'>Ram Bar</NavbarBrand>
                     <Nav className="ml-auto" navbar >
                         <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/'>Home</NavLink>
+                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/'><i class="fas fa-home"></i> Home</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/about-us'>About Us</NavLink>
+                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/about-us'><i class="fas fa-at"></i> About Us</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/menu'>Menu</NavLink>
+                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/menu'><i class="fas fa-glass-cheers"></i> Menu</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/games'>Games</NavLink>
+                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/games'><i class="fas fa-dice"></i> Games</NavLink>
                         </NavItem>
                         <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/partners'>Partners</NavLink>
+                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/partners'><i class="far fa-handshake"></i> Partners</NavLink>
                         </NavItem>
                     </Nav>
             </Navbar>
