e783c9f48d6551c5df2eafc03de5bcac89835eb8
Nick Ramsey <nramseysc@gmail.com>
2020-07-28 21:20:43 -0700

    restyle navbar and add toggle fucntion for small screens

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,51 +1,77 @@
 import React from 'react';
 import './TopNav.css';
 import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem,
-  } from 'reactstrap';
-import {Link} from 'react-router-dom';
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class TopNav extends React.Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
 
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
 
-    render(){
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
 
-        return(<div>
-            <Navbar color="light" light expand="md">
-                <NavbarBrand style={{fontWeight: "bold"}} href='/'>Ram Bar</NavbarBrand>
-                    <Nav className="ml-auto" navbar >
-                        <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/'><i class="fas fa-home"></i> Home</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/about-us'><i class="fas fa-at"></i> About Us</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/menu'><i class="fas fa-glass-cheers"></i> Menu</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/games'><i class="fas fa-dice"></i> Games</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink style={{color: 'black', fontWeight: "bold"}} href='/partners'><i class="far fa-handshake"></i> Partners</NavLink>
-                        </NavItem>
-                    </Nav>
-            </Navbar>
-        </div>);
-    }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand style={{ fontWeight: 'bold' }} href="/">
+            Ram Bar
+          </NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink style={{ color: 'white' }} href="/">
+                  <i class="fas fa-home"></i> Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{ color: 'white' }} href="/about-us">
+                  <i class="fas fa-at"></i> About Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{ color: 'white' }} href="/menu">
+                  <i class="fas fa-glass-cheers"></i> Menu
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{ color: 'white' }} href="/games">
+                  <i class="fas fa-dice"></i> Games
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink style={{ color: 'white' }} href="/partners">
+                  <i class="far fa-handshake"></i> Partners
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default TopNav;
