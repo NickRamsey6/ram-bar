3cc107fb31a5cb37963486082bb4646afe5934d9
Nick Ramsey <nramseysc@gmail.com>
2020-07-29 22:14:32 -0700

    restyle footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import './Footer.css';
+import { Container, Row, Col } from 'reactstrap';
 
-class Footer extends React.Component{
-    constructor(props) {
-        super(props);
-    }
+class Footer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        return <footer>
-            <h6>Ram Bar is a subsidiary of RamFam Holdings LLC</h6>
-        </footer>
-    }
+  render() {
+    return (
+      <footer id="main-footer" class="text-center p-4">
+        <Container>
+          <Row>
+            <Col>
+              <h6>Ram Bar is a subsidiary of RamFam Holdings LLC</h6>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
