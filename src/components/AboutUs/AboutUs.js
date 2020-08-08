f31014652f0021b2044792f3301a05af3113f169
Nick Ramsey <nramseysc@gmail.com>
2020-08-07 22:47:52 -0700

    fix stickiness of navbar by removing div it was in

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import './AboutUs.css';
 import SiteCarousel from '../SiteCarousel';
+import { Container, Jumbotron, Row, Col } from 'reactstrap';
 
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-class AboutUs extends React.Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return(
-            <div className='container' >
-                <h1>About Ram Bar</h1>
-                <SiteCarousel />
-                <p>Lorem IpsumWhat is Lorem Ipsum?
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <Container className="py-3">
+        <Row>
+          <Col md={6}>
+            <h1>Our Bar is Our Family</h1>
+            <p>
+              Lorem IpsumWhat is Lorem Ipsum? Lorem Ipsum is simply dummy text
+              of the printing and typesetting industry. Lorem Ipsum has been the
+              industry's standard dummy text ever since the 1500s, when an
+              unknown printer took a galley of type and scrambled it to make a
+              type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially
+              unchanged. It was popularised in the 1960s with the release of
+              Letraset sheets containing Lorem Ipsum passages, and more recently
+              with desktop publishing software like Aldus PageMaker including
+              versions of Lorem Ipsum.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default AboutUs;
--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -33,43 +33,41 @@ class TopNav extends React.Component {
 
   render() {
     return (
-      <div>
-        <Navbar color="dark" dark expand="md" fixed="top">
-          <NavbarBrand style={{ fontWeight: 'bold' }} href="/">
-            Ram Bar
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink style={{ color: 'white' }} href="/">
-                  <i class="fas fa-home"></i> Home
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink style={{ color: 'white' }} href="/about-us">
-                  <i class="fas fa-at"></i> About Us
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink style={{ color: 'white' }} href="/menu">
-                  <i class="fas fa-glass-cheers"></i> Menu
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink style={{ color: 'white' }} href="/games">
-                  <i class="fas fa-dice"></i> Games
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink style={{ color: 'white' }} href="/partners">
-                  <i class="far fa-handshake"></i> Partners
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      <Navbar color="dark" dark expand="md" sticky={'top'}>
+        <NavbarBrand style={{ fontWeight: 'bold' }} href="/">
+          Ram Bar
+        </NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink style={{ color: 'white' }} href="/">
+                <i class="fas fa-home"></i> Home
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{ color: 'white' }} href="/about-us">
+                <i class="fas fa-at"></i> About Us
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{ color: 'white' }} href="/menu">
+                <i class="fas fa-glass-cheers"></i> Menu
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{ color: 'white' }} href="/games">
+                <i class="fas fa-dice"></i> Games
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink style={{ color: 'white' }} href="/partners">
+                <i class="far fa-handshake"></i> Partners
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     );
   }
 }
